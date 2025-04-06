a612ae57c9a5c650d232089c05f1376941f40e96
Achraf bouziane <161948424+achrafbouziane@users.noreply.github.com>
2025-04-06 15:52:46 +0100

    fix 1
    fix postgresql

--- src/main/java/com/platform/survey/entites/Question.java
+++ src/main/java/com/platform/survey/entites/Question.java
@@ -1,24 +1,26 @@
 package com.platform.survey.entites;
+
 import com.platform.survey.enums.TypeQuestion;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import javax.swing.text.html.Option;
 import java.util.List;
-
 
 @Entity
 @Data
 public class Question {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String texte;
+
     @Enumerated(EnumType.STRING)
     private TypeQuestion type;
+
     @ManyToOne
     private Sondage sondage;
+
     @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     private List<Option> options;
-
-
 }
